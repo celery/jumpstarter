34d49f3d0e9be017d79d7f73e9c19b2f4a070d5d
Omer Katz <omer.drow@gmail.com>
2021-01-06 17:03:51 +0200

    Begin work on actor.

---
+++ jumpstarter/resources.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+import typing
+from contextlib import AsyncExitStack
+from functools import wraps
+
+import anyio
+
+__all__ = ("NotAResourceError",)
+
+from jumpstarter.states import ActorStartingState
+
+
+class NotAResourceError(Exception):
+    def __init__(self, resource_callback: typing.Callable, return_value):
+        super(NotAResourceError, self).__init__(
+            f"The return value of {resource_callback.__name__} is not a context manager.\n"
+            f"Instead we got {return_value}."
+        )
+
+
+class Resource:
+    def __init__(self, resource_callback: typing.Callable, timeout: float = None):
+        self._resource_callback = resource_callback
+        self._timeout = timeout
+
+    def __set_name__(self, owner, name):
+        print(owner)
+        if self._timeout:
+
+            @wraps(self._resource_callback)
+            async def resource_acquirer(event_data):
+                self_ = event_data.model
+                resource = self._resource_callback(self_)
+
+                try:
+                    async with anyio.fail_after(self._timeout):
+                        await self_._exit_stack.enter_async_context(resource)
+                except AttributeError as e:
+                    raise NotAResourceError(self_._resource_callback, resource) from e
+
+        else:
+
+            @wraps(self._resource_callback)
+            async def resource_acquirer(event_data):
+                self_ = event_data.model
+                resource = self._resource_callback(self_)
+
+                try:
+                    await self_._exit_stack.enter_async_context(resource)
+                except AttributeError as e:
+                    raise NotAResourceError(self._resource_callback, resource) from e
+
+        transition = owner._state_machine.get_transitions(
+            "start",
+            ActorStartingState.dependencies_started,
+            ActorStartingState.resources_acquired,
+        )[0]
+        transition.before.append(resource_acquirer)
+
+        setattr(owner, name, self._resource_callback)
