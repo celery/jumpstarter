873393bb202e9a2e609c735eaf10076d218d25b5
Omer Katz <omer.drow@gmail.com>
2021-01-06 19:18:19 +0200

    All unskipped tests are passing.

--- jumpstarter/actors.py
+++ jumpstarter/actors.py
@@ -1,4 +1,5 @@
 import typing
+from collections import defaultdict
 from contextlib import AsyncExitStack
 from functools import partial, wraps
 from typing import ClassVar
@@ -26,18 +27,23 @@ NestedState.separator = "↦"
 
 
 class Actor:
-    _state_machine: ClassVar[ActorStateMachine] = ActorStateMachine()
+    __state_machine: ClassVar[
+        typing.Dict[typing.Type, ActorStateMachine]
+    ] = defaultdict(ActorStateMachine)
+
+    @classmethod
+    @property
+    def _state_machine(cls) -> ActorStateMachine:
+        return cls._Actor__state_machine[cls]
 
     def __init__(self):
-        cls = type(self)
+        cls: Type = type(self)
         cls._state_machine.add_model(self)
 
         self._exit_stack: AsyncExitStack = AsyncExitStack()
         self._cancel_scope: CancelScope = anyio.open_cancel_scope()
 
     def __init_subclass__(cls, **kwargs):
-        cls._state_machine = ActorStateMachine()
-
         for base in cls.__bases__:
             base_state_machine = getattr(base, "_state_machine", None)
 
@@ -54,51 +60,20 @@ class Actor:
                     base_state_machine.get_transitions(),
                 ):
                     this_transition.prepare.extend(that_transition.prepare)
-                    this_transition.before.extend(that_transition.before)
-                    this_transition.after.extend(
-                        filter(lambda x: x not in ("start", "stop"), that_transition.after)
+                    this_transition.before.extend(
+                        list(
+                            filter(
+                                lambda x: getattr(x, "__name__", None)
+                                != "_release_resources",
+                                that_transition.before,
+                            )
+                        )
                     )
-
-    @classmethod
-    def acquire_resource(
-        cls,
-        resource_callback: typing.Optional[typing.Callable] = None,
-        *,
-        timeout: typing.Optional[float] = None,
-    ) -> typing.Callable:
-        if resource_callback is None:
-            return partial(cls.acquire_resource, timeout=timeout)
-
-        if timeout:
-
-            @wraps(resource_callback)
-            async def resource_acquirer(event_data):
-                self = event_data.model
-                resource = resource_callback(self)
-
-                try:
-                    async with anyio.fail_after(timeout):
-                        await self._exit_stack.enter_async_context(resource)
-                except AttributeError as e:
-                    raise NotAResourceError(resource_callback, resource) from e
-
-        else:
-
-            @wraps(resource_callback)
-            async def resource_acquirer(event_data):
-                self = event_data.model
-                resource = resource_callback(self)
-
-                try:
-                    await self._exit_stack.enter_async_context(resource)
-                except AttributeError as e:
-                    raise NotAResourceError(resource_callback, resource) from e
-
-        transition = cls._state_machine.get_transitions(
-            "start",
-            ActorStartingState.dependencies_started,
-            ActorStartingState.resources_acquired,
-        )[0]
-        transition.before.append(resource_acquirer)
-
-        return resource_callback
+                    this_transition.after.extend(
+                        list(
+                            filter(
+                                lambda x: x not in ("start", "stop"),
+                                that_transition.after,
+                            )
+                        )
+                    )
