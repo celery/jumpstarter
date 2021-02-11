4cb3b6cce5568c7065de50155f1ce5134859f753
Omer Katz <omer.drow@gmail.com>
2021-02-11 15:00:58 +0200

    Skip test if diagrams are not supported due to missing extra dependency.

--- tests/actors/test_state_machine_diagram.py
+++ tests/actors/test_state_machine_diagram.py
@@ -1,10 +1,12 @@
 import pytest
 
 from jumpstarter.actors import Actor
+from jumpstarter.states import diagrams
 
 pytestmark = pytest.mark.anyio
 
 
+@pytest.mark.skipif(not diagrams, reason="pygraphviz is not installed")
 async def test_draw_state_machine_diagram():
     actor = Actor()
     actor.draw_state_machine_graph("state_machine.png")
